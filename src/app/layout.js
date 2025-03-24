09c26af52fa1387c3185d929c481bd3d047501bf
Sandhya630 <sandhyaksandy993@gmail.com>
2025-03-24 14:30:47 +0530

    footer head

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,14 @@
 import "./globals.css";
 import Header from "@/app/components/Header";
+import Footer from "./components/footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-100">
         <Header />
+        <Footer />
+
         <main className="p-6">{children}</main>
       </body>
     </html>
